13a575ec7b50d0280302427f8691c90f348f86b2
Shereink <104611538+Shereink@users.noreply.github.com>
2022-11-30 23:50:16 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,130 @@
+
+
+// task one ///////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+
+// var temp = prompt("Enter Temprature");
+// var temp = parseInt(temp);
+// temp >30 ? alert(" hot!!") : alert("cold");
+// if (temp >30){
+
+//     document.write("Hot!!")
+// }
+
+// else {
+//     document.write("Cold..")
+// }
+
+
+// task two //////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// var name=prompt(" Enter your name :");
+// if (isNaN(name)){
+
+//     var birth =prompt("Enter birth date :")
+
+// }
+// else{ 
+
+//     for(i=0 ; i < 2 ; i ++){
+//     var name=prompt(" Enter right name !!:")
+//     !isNaN(name)
+// }}
+       
+    
+
+
+//     if(isFinite(birth)< 2010){
+//         age=birth-2010
+//     }
+    
+//     else{
+//         for(i=0 ; i < 2 ; i ++){
+//             var name=prompt(" Enter right birth date ! :")
+    
+//         }}
+
+
+
+
+    
+
+// task three /////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+// quadratic equation
+
+
+// let root1, root2;
+
+// // take input from the user
+// let a = prompt("Enter the first number: ");
+// let b = prompt("Enter the second number: ");
+// let c = prompt("Enter the third number: ");
+
+// // calculate discriminant
+// let discriminant = b * b - 4 * a * c;
+
+// // condition for real and different roots
+// if (discriminant > 0) {
+//     root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+//     root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+    
+//     prompt (root1 , root2)
+// }
+
+// // condition for real and equal roots
+// else if (discriminant == 0) {
+//     root1 = root2 = -b / (2 * a);
+
+    
+//     prompt (root1 , root2)
+// }
+
+// // if roots are not real
+// else {
+//     let realPart = (-b / (2 * a)).toFixed(2);
+//     let imagPart = (Math.sqrt(-discriminant) / (2 * a)).toFixed(2);
+
+    
+//     prompt(
+//         realPart + imagPart  , realPart - imagPart 
+//       );
+// }
+
+
+
+
+// task four ////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// var num1 =prompt("Enter num1 : ")
+// var num2 =prompt("Enter num2 : ")
+// var x = num1.toString();
+// var y = num2.toString();
+// console.log( x.slice(length-1))
+// console.log( y.slice(length-1))
+// var z = parseInt (x.slice(length-1))
+// var s= parseInt (y.slice(length-1))
+// sum=z+s;
+// console.log(sum);
+
+
+
+///task five ////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// var distance = prompt("Enter Distence :")
+// time=distance*2
+// alert( " the time is " + time);
+
+
+
+
+
+
+
+
