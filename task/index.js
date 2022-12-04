a9c0096f0234aade9596e6ca718c8046f022f726
Shereink <104611538+Shereink@users.noreply.github.com>
2022-12-04 23:40:06 +0200

    Add files via upload

---
+++ task/index.js
@@ -0,0 +1,98 @@
+
+
+// task one /////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+// var x;
+// function newwindow() {
+//     x = window.open("https://www.google.com/", "", "_blank",
+//         config = 'height=60px,width=400');
+// }
+// function closewindow() {
+//     x.close();
+// }
+
+// document.getElementById('close'),addEventListener ("click" , closewindow);
+
+
+// task two ////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// function show(){
+
+//     alert("CLOCK STARTRD !")
+//     settime();
+
+//     function settime(){
+//         var d= new Date
+//         var s= d.getSeconds();
+//         var m= d.getMinutes();
+//         var h= d.getHours();
+//         span.textContent = 
+//             ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
+//       };
+//       setInterval(settime,1000);
+
+// }
+// var span = document.getElementById('span');
+
+
+//task three //////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+//task four //////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// function create(){
+//   var x=  window.open ("page.html","","_blank")
+//   x.scroll(100,100)   
+//   function scorling()
+// {
+//     if(scrollY>777)
+//     {
+//         alert ("hi")
+//     }
+// }}
+
+
+//task five //////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+
+// function OpenWindow() { window.open('page.html','newwindow',
+//              config='height=670,width=1400')
+// settimeout( function(){document.location.href='www.google.com'}, 1000 );
+// }
+
+//task six //////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+// var popupWindow=null;
+
+// function popup()
+// {
+//     popupWindow = window.open('page.html','name','width=200,height=200');
+// }
+
+// function parent_disable() {
+// if(popupWindow && !popupWindow.closed)
+// popupWindow.focus();
+// }
+
+// // var popupWindow=null;
+
+// function child_open()
+// { 
+
+// popupWindow =window.open('',"_blank","directories=no, status=no, menubar=no, scrollbars=yes, resizable=no,width=600, height=280,top=200,left=200");
+
+// }
+// function parent_disable() {
+// if(popupWindow && !popupWindow.closed)
+// popupWindow.focus();
+// }
